af06ae59d4dfd0c3ee0c05132577f1d0541c7a3d
Arnoldas Seputis <arnoldas.seputis@esrlabs.com>
2022-12-09 14:16:04 +0100

    [day9] Refactor the code
    Dried up and commented the code.
    
    Also the code was tested on an input from another account,
    and in that case it provided an answer that was 1 higher
    than the correct answers:
          Actual   Correct
    Part1  6340     6339
    Part2  2542     2541
    
    The discrepancy suggest a small bug, needs to be found.

--- day9/run.rb
+++ day9/run.rb
@@ -1,136 +1,132 @@
 # frozen_string_literal: true
 
-input = File.read('./input.txt')
+DIRS = { # Directions, Right means +1 on x axis.
+  'R' => [ 1, 0],
+  'L' => [-1, 0],
+  'U' => [ 0, 1],
+  'D' => [ 0,-1]
+}.freeze
 
-DIRS = {
-  'R' => [1, 0],
-  'L' => [-1 ,0],
-  'U' => [0, 1],
-  'D' => [0, -1]
-}
+# Adjusts the position of the adjacent knot +t_pos+.
+# @param [Array(Integer, Integer)] h_pos The reference knot
+# @param [Array(Integer, Integer)] t_pos The target knot, whose position
+#   needs to be adjusted with respect to +h_pos+
+def adjust(h_pos, t_pos)
+  x_diff = h_pos[0] - t_pos[0]
+  y_diff = h_pos[1] - t_pos[1]
+  pos_diff = [x_diff, y_diff]
+  longer_i = pos_diff.find_index do |x|
+    x.abs == 2
+  end
+
+  # validation
+  pos_diff.each { |x| raise(StandardError, 'The tail got lost') if x.abs > 2 }
+
+  return unless longer_i
+
+  shortr_i = longer_i == 1 ? 0 : 1
+  scale = pos_diff[shortr_i].abs == 2 ? 2 : 1
+
+  t_pos[longer_i] = t_pos[longer_i] +  pos_diff[longer_i] / 2
+  t_pos[shortr_i] = t_pos[shortr_i] + (pos_diff[shortr_i] / scale)
+end
+
+# Based on +movements+ and +knots+ positions, it updates the +t_hist+ Hash
+# with the unique positions that it has.
+# @param [Array<Array(Integer, Integer)>] movements An array of movements
+#   for the first knot in +knots+. Each element is an array, with displacements
+#   in x and y positions.
+# @param [Array<Array(Integer, Integer)>] knots An array of consecutive knots.
+#   The first knot is the HEAD, the last knot is the TAIL.
+# @param [Hash] t_hist A Hash that tracks the position of the last knot.
+def track_tail(movements, knots, t_hist)
+  movements.each do |x_diff, y_diff|
+    knots.first[0] += x_diff
+    knots.first[1] += y_diff
+    (knots.size - 2).times.map do |i|
+      adjust(knots[i], knots[i + 1])
+    end
+    adjust(knots[-2], knots[-1])
+    t_hist[knots.last.dup] ||= 1
+  end
+end
+
+# Based on +movements+ and +nr_knots+, it calculates how many
+# unique positions the TAIL (last element in +knots+) has, provided
+# that all knots start in the same position.
+# @param [Array<Array(Integer, Integer)>] movements An array of movements
+#   for the first knot in +knots+. Each element is an array, with displacements
+#   in x and y positions.
+# @param [Integer] nr_knots The number of knots in a row
+# @return [Integer] The number of unique positions that the TAIL in the rope
+#   assumes after the HEAD executes the +movements+
+def nr_slots(movements, nr_knots)
+  # first knot is the head, last knot is the tail
+  knots = nr_knots.times.map do
+    [0, 0]
+  end
+  t_hist = {}
+
+  track_tail(movements, knots, t_hist)
+  t_hist.size + 1
+end
+
+# PROCESS INPUT ---- START -----
+input = File.read('./ls_input.txt')
 
 movements = input.split("\n").flat_map do |row|
   direction, steps = row.split(' ')
   Array.new(steps.to_i, DIRS[direction])
 end
+# @example movements
+# [
+#  [ 0, 1], # one up
+#  [ 0, 1], # one up
+#  [-1, 0], # one to the left
+#    ...    # ...
+# ]
+#
+# PROCESS INPUT ---- FINISH -----
 
-h_pos = [0, 0]
-t_pos = [0, 0]
-
-t_hist = {}
-
-def adjust(h_pos, t_pos, t_hist)
-  x_diff = h_pos[0] - t_pos[0]
-  y_diff = h_pos[1] - t_pos[1]
-  pos_diff = [x_diff, y_diff]
-  longer_i = pos_diff.find_index do |x|
-    x.abs == 2
-  end
-
-  return unless longer_i
-
-  shortr_i = longer_i == 1 ? 0 : 1
-
-  t_pos[longer_i]  = t_pos[longer_i] + pos_diff[longer_i] / 2
-  t_pos[shortr_i]  = t_pos[shortr_i] + pos_diff[shortr_i]
-
-  t_hist[t_pos.dup] ||= 1
-end
-
-movements.each do |x_diff, y_diff|
-  h_pos[0] += x_diff
-  h_pos[1] += y_diff
-  adjust(h_pos, t_pos, t_hist)
-end
-
-puts t_hist.size + 1
-
+# PART 1
+puts nr_slots(movements, 2)
 # PART 2
+puts nr_slots(movements, 10)
 
-def adjust(h_pos, t_pos, t_hist = nil)
-  x_diff = h_pos[0] - t_pos[0]
-  y_diff = h_pos[1] - t_pos[1]
-  pos_diff = [x_diff, y_diff]
-  longer_i = pos_diff.find_index do |x|
-    x.abs == 2
-  end
+# P1: 6391
+# P2: 2593
 
-  longest_i = pos_diff.find_index do |x|
-    x.abs > 2
-  end
 
-  require 'pry'
-  binding.pry if longest_i
-  
 
-  return unless longer_i
 
-  shortr_i = longer_i == 1 ? 0 : 1
 
-  t_pos[longer_i]  = t_pos[longer_i] + pos_diff[longer_i] / 2
 
-  #binding.pry if 
-  if pos_diff[shortr_i].abs == 2
 
-    t_pos[shortr_i] = t_pos[shortr_i] + (pos_diff[shortr_i] / 2)
-  else
-    t_pos[shortr_i] = t_pos[shortr_i] + pos_diff[shortr_i]
-  end
-  #t_pos[shortr_i] = t_pos[shortr_i] + pos_diff[shortr_i]
 
-  #require 'pry'
-  #binding.pry
-  if t_hist
-    t_hist[t_pos.dup] ||= 0
-    t_hist[t_pos.dup] += 1
-  end
-end
 
-h_pos = [0, 0]
-pos_1 = [0, 0]
-pos_2 = [0, 0]
-pos_3 = [0, 0]
-pos_4 = [0, 0]
-pos_5 = [0, 0]
-pos_6 = [0, 0]
-pos_7 = [0, 0]
-pos_8 = [0, 0]
-t_pos = [0, 0]
 
-t_hist = {}
 
-movements.each do |x_diff, y_diff|
-  h_pos[0] += x_diff
-  h_pos[1] += y_diff
-  adjust(h_pos, pos_1)
-  adjust(pos_1, pos_2)
-  adjust(pos_2, pos_3)
-  adjust(pos_3, pos_4)
-  adjust(pos_4, pos_5)
-  adjust(pos_5, pos_6)
-  adjust(pos_6, pos_7)
-  adjust(pos_7, pos_8)
-  adjust(pos_8, t_pos, t_hist)
-end
 
-puts t_hist.size + 1        # unique values
-puts t_hist.values.sum + 1  # non-unique values
 
-# i guessed 2252 (unique) and 2340 (not unique)
 
-# Prints the places where the tail has been.
-def print_(hist)
+
+# BONUS:
+# Prints the places where the tail has been. Same format as in the example
+def print_tail_hist(hist)
   size = 40
   screen = size.times.map do |_row|
     Array.new(size, '.')
   end
 
   hist.each do |pos|
-    screen[-(pos.first[1]) - size / 2][(pos.first[0]) - size / 2] = '#'
+    screen[-pos.first[1] - size / 2][pos.first[0] - size / 2] = '#'
   end
   screen[size / 2][size / 2] = 'S'
   screen.each do |row|
-    print row.join('') + "\n"
+    print "#{row.join('')}\n"
   end
 end
-# print_(t_hist) It shows the correct picture, just like in the example
+# print_tail_hist(t_hist)
+
+# ls p1: 6340 (too high) --> 6339 worked!
+# ls p2: 2542 (too high) --> 2541 worked!
