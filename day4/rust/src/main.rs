03edc843b1b1f6be2894fa4daccf94db661678f8
Arnoldas Seputis <arnoldas.seputis@esrlabs.com>
2022-12-19 19:03:32 +0100

    [day4][rust] Refactor Part 2 solution

--- day4/rust/src/main.rs
+++ day4/rust/src/main.rs
@@ -2,53 +2,46 @@ use std::fs;
 use array_tool::vec::Intersect;
 
 fn main() {
-    let contents: String = fs::read_to_string("../input.txt")
+    let contents: String = fs::read_to_string("../ls_input.txt")
         .expect("Should have been able to read the file");
 
     let split: Vec<&str> = contents.split("\n").collect();
     
+    let separator_0: &str = "-";
+    let separator_1: &str = ",";
+
     let mut nr_overlap = 0;
 
     for row in split {
-        // println!("{}", row);
-        let row_elements: Vec<&str> = row.split(",").collect();
-        let first: &str = row_elements[0];
-        let last:  &str = row_elements[1];
+        let row_elements: Vec<&str>  = split_custom(&row, &separator_1);
 
-        let first_elements: Vec<&str> = first.split("-").collect();
-        let first_start: &str = first_elements[0];
-        let first_end:   &str = first_elements[1];
-        let first_start_int: i32 = first_start.parse::<i32>().unwrap();
-        let first_end_int:   i32 =   first_end.parse::<i32>().unwrap();
-
-        let first_range: Vec<i32> = (first_start_int..=first_end_int).collect();
-
-        let last_elements: Vec<&str> = last.split("-").collect();
-        let last_start: &str = last_elements[0];
-        let last_end:   &str = last_elements[1];
-        let last_start_int: i32 = last_start.parse::<i32>().unwrap();
-        let last_end_int:   i32 =   last_end.parse::<i32>().unwrap();
-
-        let last_range: Vec<i32> = (last_start_int..=last_end_int).collect();
-
-        //  println!("{:?}", first_range);
-        //  println!("{:?}", last_range);
-        // println!("{}", vec![1,2,3].intersect(vec![3,4,5]).len());
-        // println!("{}", first_range.intersect(last_range).len());
+        let first_range:  Vec<i32>   = str_to_range(row_elements[0], &separator_0);
+        let last_range:   Vec<i32>   = str_to_range(row_elements[1], &separator_0);
+        // println!("{:?}", first_range);
 
         if first_range.intersect(last_range).len() > 0 {
             nr_overlap += 1
         }
-
     }
-
     println!("{}", nr_overlap)
 }
 
-// fn split_by(string: &String, by: &str) -> Vec<&str> {
-//     return string.split(by).collect();
-// }
+// Splits the 'string' into an array with a 'separator'
+fn split_custom<'a>(string: &'a str, separator: &'a str) -> Vec<&'a str> {
+    return string.split(separator).collect();
+}
 
-// fn get_range(start: &str, end: &str) -> String {
-//    return String::from("hello");
-// }
+// Creates an array of integers between 'start' and 'end'
+fn gen_range(start: &str, end: &str) -> Vec<i32> {
+    let start_int: i32 = start.parse::<i32>().unwrap();
+    let end_int:   i32 =   end.parse::<i32>().unwrap();
+    return (start_int..=end_int).collect();
+}
+
+// Creates an array of integers as dictated in the 'range_str'
+// (e.g input "3-6")
+// (e.g output [3,4,5,6])
+fn str_to_range(range_str: &str, separator: &str) -> Vec<i32> {
+    let first_elements: Vec<&str>  = split_custom(range_str, &separator);
+    return gen_range(first_elements[0], first_elements[1]);
+}
