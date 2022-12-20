4748dfa157a73e1cffd1ab897c0785c4e81de666
Arnoldas Seputis <arnoldas.seputis@esrlabs.com>
2022-12-20 08:14:46 +0100

    [day4][rust] Optimize the code
    HashSet intersection is now used.

--- day4/rust/src/main.rs
+++ day4/rust/src/main.rs
@@ -1,12 +1,14 @@
 use std::fs;
-use array_tool::vec::Intersect;
+use std::collections::HashSet;
+use std::time::Instant;
 
 fn main() {
+    let start = Instant::now();
     let contents: String = fs::read_to_string("../ls_input.txt")
         .expect("Should have been able to read the file");
 
     let split: Vec<&str> = contents.split("\n").collect();
-    
+    // println!("{:?}", split);
     let separator_0: &str = "-";
     let separator_1: &str = ",";
 
@@ -15,15 +17,18 @@ fn main() {
     for row in split {
         let row_elements: Vec<&str>  = split_custom(&row, &separator_1);
 
-        let first_range:  Vec<i32>   = str_to_range(row_elements[0], &separator_0);
-        let last_range:   Vec<i32>   = str_to_range(row_elements[1], &separator_0);
-        // println!("{:?}", first_range);
+        let first_range:  HashSet<i32>   = str_to_range(row_elements[0], &separator_0);
+        let last_range:   HashSet<i32>   = str_to_range(row_elements[1], &separator_0);
+        //println!("{:?}", row_elements);
 
-        if first_range.intersect(last_range).len() > 0 {
+        if first_range.intersection(&last_range).count() > 0 {
             nr_overlap += 1
         }
     }
-    println!("{}", nr_overlap)
+    println!("{}", nr_overlap);
+
+    let duration = start.elapsed();
+    println!("Time elapsed is: {:?}", duration);
 }
 
 // Splits the 'string' into an array with a 'separator'
@@ -32,16 +37,16 @@ fn split_custom<'a>(string: &'a str, separator: &'a str) -> Vec<&'a str> {
 }
 
 // Creates an array of integers between 'start' and 'end'
-fn gen_range(start: &str, end: &str) -> Vec<i32> {
+fn gen_range(start: &str, end: &str) -> HashSet<i32> {
     let start_int: i32 = start.parse::<i32>().unwrap();
     let end_int:   i32 =   end.parse::<i32>().unwrap();
-    return (start_int..=end_int).collect();
+    return HashSet::from_iter(start_int..=end_int);
 }
 
 // Creates an array of integers as dictated in the 'range_str'
 // (e.g input "3-6")
 // (e.g output [3,4,5,6])
-fn str_to_range(range_str: &str, separator: &str) -> Vec<i32> {
+fn str_to_range(range_str: &str, separator: &str) -> HashSet<i32> {
     let first_elements: Vec<&str>  = split_custom(range_str, &separator);
     return gen_range(first_elements[0], first_elements[1]);
 }
